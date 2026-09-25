82951b1176bf6ff01b81b2812d59109bb4c2cba2
Antumbra-Mind <maxsebb.11@gmail.com>
2026-09-25 22:37:38 +0300

    AircraftInput Adds
    Додав та розібрався як оптимізувати інпут з клавіш.
    Щоб не було через Update.
    А саме додав:
    Pitch
    Yaw
    Roll
    Throttle

--- ProjectF/Assets/Scripts/PlanesScripts/AircraftInput.cs
+++ ProjectF/Assets/Scripts/PlanesScripts/AircraftInput.cs
@@ -8,16 +8,56 @@ using UnityEngine.InputSystem;
 public class AircraftInput : MonoBehaviour
 {
     private AircraftInputActions inputAction;
+    private float pitch;
+    private float roll;
+    private float yaw;
+    private float throttle;
+
+    public float Pitch => pitch;
+    public float Roll => roll;
+    public float Yaw => yaw;
+    public float Throttle => throttle; 
 
     private void Awake()
     {
         inputAction = new AircraftInputActions();
         inputAction.Enable();
+
+        inputAction.Flight.Pitch.performed += OnPitch;
+        inputAction.Flight.Pitch.canceled += OnPitch;
+
+        inputAction.Flight.Roll.performed += OnRoll;
+        inputAction.Flight.Roll.canceled += OnRoll;
+
+        inputAction.Flight.Yaw.performed += OnYaw;
+        inputAction.Flight.Yaw.canceled += OnYaw;
+
+        inputAction.Flight.Throttle.performed += OnThrottle;
+        inputAction.Flight.Throttle.canceled += OnThrottle;
     }
 
-    private void Update()
+
+    private void OnPitch(InputAction.CallbackContext context)
     {
-        float pitch = inputAction.Flight.Pitch.ReadValue<float>();
-        Debug.Log(pitch);
+        pitch = context.ReadValue<float>();
+        Debug.Log($"Pitch: {pitch}");
     }
+
+    private void OnRoll(InputAction.CallbackContext context)
+    {
+        roll = context.ReadValue<float>();
+        Debug.Log($"Roll: {roll}");
+    }
+
+    private void OnYaw(InputAction.CallbackContext context)
+    {
+        yaw = context.ReadValue<float>();
+        Debug.Log($"yaw: {yaw}");
+    }
+
+    private void OnThrottle(InputAction.CallbackContext context)
+    {
+        throttle = context.ReadValue<float>();
+    }
+
 }
